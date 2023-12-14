a6bf970906f5e9f0b1a1468ab7b4ce5f8ef32924
pks21g <paulsmith414@yahoo.com>
2023-12-14 16:15:53 -0600

    implemented read.md step IV dynamic page using js

---
+++ frontend/src/data.js
@@ -0,0 +1,64 @@
+export default { 
+    products: [
+    {
+        _id: '1',
+        name: 'Nike Long Sleeve workout',
+        image: 'images/product-1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 20,
+        countInStock: 6
+    },
+    {
+        _id: '2',
+        name: 'RL Polo Tshirt',
+        image: 'images/product-2.jpg',
+        price: 100,
+        brand: 'Ralph Lauren',
+        rating: 4.5,
+        numReviews: 20,
+        countInStock: 3
+    },
+    {
+        _id: '3',
+        name: 'Levis button down Blue',
+        image: 'images/product-3.jpg',
+        price: 40,
+        brand: 'Levis',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 6
+    },
+    {
+        _id: '4',
+        name: 'Gap Custom Slim fit Checkered Shirt',
+        image: 'images/product-4.jpg',
+        price: 45,
+        brand: 'Gap',
+        rating: 4.5,
+        numReviews: 20,
+        countInStock: 6
+    },
+    {
+        _id: '5',
+        name: 'Holiester Summer Tee Lime Green',
+        image: 'images/product-5.jpg',
+        price: 24,
+        brand: 'Holiester',
+        rating: 4.5,
+        numReviews: 20,
+        countInStock: 6
+    },
+    {
+        _id: '6',
+        name: 'Banana Repubic Plain Button Down White',
+        image: 'images/product-6.jpg',
+        price: 94,
+        brand: 'Banana Republic',
+        rating: 4.5,
+        numReviews: 20,
+        countInStock: 6
+    }
+]
+}
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,33 @@
+import data from '../data.js'
+
+const HomeScreen = {
+  render: () => {
+    const {products} = data;
+    if (products === 'undefined') return null;
+    else{
+    return  `<ul class="products">
+            ${products.map(
+                
+              product => `<li>
+                    <div class="product">
+                        <a href="/#/product/${product._id}">
+                            <img src="${product.image}" alt="${product.name}" />
+                        </a>
+                        <div class="product-name">
+                            <a href="">${product.name}</a>
+                        </div>
+                        <div class="product-brand">
+                            <a href="">${product.brand}</a>
+                        </div>
+                        <div class="product-price">
+                            <a href="">$${product.price}</a>
+                        </div>
+                    </div>
+                </li>
+                `
+            ).join('\n')}
+            </ul>`};
+  },
+};
+
+export default HomeScreen;
